82d0360f659124e2531584a92a3b9a8272ae9a9c
Xin Shi <xinshi@microsoft.com>
2020-01-16 14:33:01 -0800

    Separate output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh -l
 
 echo "Run InferSharp translation..."      
-sudo dotnet /app/Cilsil/bin/Debug/netcoreapp2.2/Cilsil.dll translate ConsoleApp1/ConsoleApp1/bin --outcfg cfg.json --outtenv tenv.json
+sudo dotnet /app/Cilsil/bin/Debug/netcoreapp2.2/Cilsil.dll translate $1 --outcfg cfg.json --outtenv tenv.json
 sudo infer capture
 sudo mkdir infer-out/captured
 echo "Run Infer..."
 sudo infer analyzejson --debug --cfg-json cfg.json --tenv-json tenv.json
 sudo chmod -R 777 infer-out/
 
-cat -s infer-out/bugs.txt | sed -E -e '/ANALYSIS_STOP|Bad_footprint|Missing_fld|PRECONDITION_NOT_MET|Assert_failure|CLASS_CAST_EXCEPTION|Abduction_case_not_implemented|Found.[0-9]+.issues|Summary.of.the.report/,/^[[:space:]]*$/d'
+var=$(cat -s infer-out/bugs.txt | sed -E -e '/ANALYSIS_STOP|Bad_footprint|Missing_fld|PRECONDITION_NOT_MET|Assert_failure|CLASS_CAST_EXCEPTION|Abduction_case_not_implemented|Found.[0-9]+.issues|Summary.of.the.report/,/^[[:space:]]*$/d')
+echo ::set-output name=results::$var
